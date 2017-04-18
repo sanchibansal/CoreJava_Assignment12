010e478500ca93ffe89dae17bfd5293f08065cb0
sanchibansal <bansalsanchi77.sb@gmail.com>
2017-04-18 09:47:49 +0530

    assignment2

---
+++ assignment2/Lecture1.java
@@ -0,0 +1,16 @@
+
+public class Lecture1 {
+	public static void main(String args[])
+	{
+	int a=20,b=10;          //assigning values of a and b
+	b=a-- - --a;		//re-assigning value of b
+	System.out.println("value of a-- - --a = " +b); //prints new value of b 
+	int c=a--;						//assigning value of c
+	System.out.println("value of a-- = "+ c);		//prints c
+	int d=a>>2;						//assigning value of d
+	System.out.println("value of a>>2 = "+d);		//prints d
+	int e= a&b;						//assigning value of e
+	System.out.println("value of a&b = " +e);		//prints e
+	
+}
+}
